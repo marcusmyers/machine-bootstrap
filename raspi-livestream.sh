156cfddbeb49738336c9409559e9d0810d7f6869
Mark Myers <marcusmyers@gmail.com>
2014-12-31 17:26:12 -0500

    Added node commands

--- raspi-livestream.sh
+++ raspi-livestream.sh
@@ -6,6 +6,8 @@ wget http://node-arm.herokuapp.com/node_latest_armhf.deb
 echo 'Install node.js...'
 sudo dpkg -i node_latest_armhf.deb
 
+echo 'Install forever globally'
+sudo npm install forever --global
 
 echo 'Make node_programs directory...'
 mkdir /home/pi/node_programs
@@ -16,4 +18,8 @@ cd /home/pi/node_programs
 echo 'Clone liveStreaming repo to Pi'
 git clone https://github.com/marcusmyers/liveStreaming.git
 
+echo 'Change directory to liveStreaming...'
+cd /home/pi/node_programs/liveStreaming
 
+echo  'Install all node modules...' 
+npm install
